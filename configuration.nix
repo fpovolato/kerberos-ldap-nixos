229af3ba9a7292011520e6723345d7ba09decfb4
Francesco <893349@stud.unive.it>
2026-06-03 18:12:34 +0200

    Phase 1: add empty srv and client containers w NAT

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,59 @@
 
   services.openssh.enable = true;
 
+  networking.nat = {
+    enable = true;
+    internalInterface = [ "ve-+" ];
+    externalInterface = "enp0s1";
+  };
+
+  networking.extraHosts = ''
+    192.168.100.11 srv srv.example.lan
+    192.168.100.12 client client.example.lan
+  '';
+
+  containers.srv = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
+
+    config =
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+      {
+        networking.useHostResolvConf = lib.mkForce false;
+        services.resolved.enable = true;
+        enviroment.systemPackages = with pkgs; [ neovim ];
+        system.stateVersion = "26.05";
+      };
+  };
+
+  containers.client = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.12";
+
+    config =
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+      {
+        networking.useHostResolvConf = lib.mkForce false;
+        services.resolved.enable = true;
+        enviroment.systemPackages = with pkgs; [ neovim ];
+        system.stateVersion = "26.05";
+      };
+  };
+
   system.stateVersion = "26.05"; # Did you read the comment?
 
 }
